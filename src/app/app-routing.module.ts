767f6a18ed8cf199197938416e4de9101cd19717
raulgh0201 <80180531+raulgh0201@users.noreply.github.com>
2023-07-06 21:35:05 +0200

    updatedAngular,changed distribution folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 
